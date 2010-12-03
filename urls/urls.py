12f9345d383b916ceae57663454fadc3ed2f6e4d
George <george.regentov@gmail.com>
2010-12-04 01:14:32 +0300

    Make zero config for btools test

---
+++ urls/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import *
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+    (r'^', include('apps.btools.urls')),
+)
