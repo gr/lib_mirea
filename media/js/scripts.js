387bd030431ac0f0703bf09b2bc8a0c3718e1f7a
Igor Spisivtsev <gexsmail@gmail.com>
2011-01-14 07:25:23 -0800

    Load More Button implemented

--- media/js/scripts.js
+++ media/js/scripts.js
@@ -25,30 +25,68 @@ $(document).ready(function() {
 
 /* CSS для неподдерживаемых ИЕ селекторов */
   if ($.browser.msie) {
-		$("div#content.simple h2 + p").css({ marginTop:"0" });
-		$("div#content.simple p + blockquote").css({ marginTop: "1.375em" });
-		$("div#content.simple ul li").prepend("&mdash;&nbsp;");
-		$("div#content p + p").css({ marginTop: "0" });
-		$("div#content p + p").css({ textIndent: "2.75em" });
-		$("div#events p + p").css({ marginTop: "0" });
-		$("div#events p + p").css({ textIndent: "2.75em" });
+      $("div#content.simple h2 + p").css({ marginTop:"0" });
+      $("div#content.simple p + blockquote").css({ marginTop: "1.375em" });
+      $("div#content.simple ul li").prepend("&mdash;&nbsp;");
+      $("div#content p + p").css({ marginTop: "0" });
+      $("div#content p + p").css({ textIndent: "2.75em" });
+      $("div#events p + p").css({ marginTop: "0" });
+      $("div#events p + p").css({ textIndent: "2.75em" });
   }
 
 /* Load More Button */
-  $("a.loadMore").click( function() {
-    $("a.loadMore").fadeOut( 50, function() {
-      $("span.loadingProgress").fadeIn(200, function() {
-        $.get("debug/tpl/search-ajax.html",function(data){          
-          $("#ecatList").append(data);
-          var countLoaded = eval( $("input#countLoaded").val() );
-          var countToLoad = eval( $("input#countToLoad").val() );
-          var totalCount = countLoaded + countToLoad;
-          $("span.countLoaded").html( totalCount );
-        },'html');
+/*
+ * Мне угрожали, когда заставляли делать пересчёт loaded и portionToLoad на клиенте!
+ */
+  var portion = 100;
+  var found = eval( $("span.countFound").html() );
+  
+  if ( found > portion ) {
+      /* Инициализация и первоначальный подсчёт значения для кнопки */
+      var q = $("#search_field").val();
+      var portionToLoad = 0;
+      var loaded = portion;
+      var more = found - loaded;
+      if ( more > portion ) {
+          portionToLoad = portion;
+      } else {
+          portionToLoad = more;
+      }
+      $("span.countToLoad").text(portionToLoad);
+      /* Повесить обработчик */
+      $("span.loadingProgress").ajaxError(function() {
+          $(this).fadeOut( 100, function() {
+              $("a.loadMore").fadeIn(100);
+          });
       });
-    });
-    return false;
-  });
+      $("a.loadMore").click( function() {
+          $("a.loadMore").fadeOut( 0, function() {
+              $("span.loadingProgress").fadeIn(100);
+          });
+          $.get("/search-more", {
+              query: q,
+              db_url: "books",
+              query_type: "CCL",
+              start: loaded,
+              len: portionToLoad
+          },function(data){          
+              $("#ecatList").append(data);
+              loaded = loaded + portionToLoad;
+              more = found - loaded;
+              if ( more > portion ) {
+                  portionToLoad = portion;
+              } else {
+                  portionToLoad = more;
+              }            
+              $("span.countToLoad").text(portionToLoad);
+              $("span.countLoaded").text(loaded);
+              $("span.loadingProgress").fadeOut( 100, function() {
+                  if (portionToLoad > 0) $("a.loadMore").fadeIn(100);
+              });
+          },'html');
+          return false;
+      });
+  }
 
 /* Special Pages Scripts */
 /* Сборник трудов 55 */
